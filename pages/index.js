21d9487b41a6fc2b5907267e3be544eca5d35cb6
clydencx <clyde@ncx.cx>
2022-03-01 12:16:11 +0400

    added the ncx logo and changed the title description

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,31 @@
+import Head from 'next/head';
 import SearchBar from '../components/SearchBar'
-export default function Home() {
+
+export default function Home({ coinsData }) {
+  console.log(coinsData);
   return (
-    <div>
-      <SearchBar type='text' placeholder='Search' />
-    </div>
+    <>
+      <Head>
+        <title>Price List</title>
+        <meta name="description" content="NCX Price List" />
+        <link rel="icon" href="/ncx.ico" />
+      </Head>
+      <div>
+        <SearchBar type='text' placeholder='Search' />
+      </div>
+    </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`);
+
+  const coinsData = await res.json();
+
+  return {
+    props: {
+      coinsData
+    }
+  }
+
 }
