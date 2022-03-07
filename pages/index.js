bc0406cedb7265f5f19b21d22c50bd3e9fbb6c0e
clydencx <clyde@ncx.cx>
2022-03-07 13:22:18 +0400

    added other 3 coins

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ import Head from 'next/head';
 import SearchBar from '../components/SearchBar'
 import TableHeader from '../components/TableHeader';
 import CoinList from '../components/CoinList';
-import BitCoin from './coins/bitcoin';
+import Eth from './coins/eth';
+import Tether from './coins/tether';
+import Binance from './coins/binance';
+import USDCoin from './coins/usdcoin';
 
 import { useState } from 'react';
 
@@ -30,7 +33,10 @@ export default function Home({ coinsData }) {
         <SearchBar type='text' placeholder='Search' onChange={handleChange} />
         <TableHeader />
         <CoinList coinsData={filteredCoins} />
-        <BitCoin />
+        <Eth />
+        <Tether />
+        <Binance />
+        <USDCoin />
       </div>
       </header>
     </>
@@ -38,7 +44,7 @@ export default function Home({ coinsData }) {
 }
 
 export const getServerSideProps = async () => {
-  const res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1&page=1&sparkline=false&price_change_percentage=1h%2C24%2C7d`);
+  const res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24%2C7d`);
 
   const coinsData = await res.json();
 
