5430aaba97794a1343308de0d20132a6ccd16fc3
clydencx <clyde@ncx.cx>
2022-03-16 10:54:17 +0400

    changed the data

--- components/TableHeader.js
+++ components/TableHeader.js
@@ -1,5 +1,7 @@
 import styles from '../styles/Coins.module.css'
 import Coins from './Coins';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleDollarToSlot, faChartColumn } from '@fortawesome/free-solid-svg-icons'
 
 const TableHeader = ({ coinsData }) => {
     return (
@@ -7,12 +9,12 @@ const TableHeader = ({ coinsData }) => {
             <div className={styles.header__container}>
                 <div className={styles.header__row}>
                     <div className={styles.coin__data_p}>
-                        <p>Pair</p>
-                        <p style={{width: "110px"}}>Last Price</p>
-                        <p>24h Change</p>
-                        <p>24h High</p>
-                        <p>24h Low</p>
-                        <p>Graph</p>
+                        <p>Name</p>
+                        <p style={{width: "110px"}}>Price</p>
+                        <p>24h%</p>
+                        <p>Market Cap <FontAwesomeIcon icon={faCircleDollarToSlot} /></p>
+                        <p>Volume(24h) <FontAwesomeIcon icon={faChartColumn} /></p>
+                        <p>Chart</p>
                         {/* <p>Action</p> */}
                     </div>
                 </div>
